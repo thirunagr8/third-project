f9130a061ece1edea72872fe4846ee5a882df3a5
Arun <Arun@Arun-PC>
2018-09-23 13:00:53 +0530

    firstpush

---
+++ src/main/java/Week0/Addition.java
@@ -0,0 +1,15 @@
+package Week0;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		//c= a+b;
+		//System.out.println("The added value of two numbers is "+ c);	
+		c= a*b;
+		System.out.println("The added value of two numbers is "+ c);	
+
+	}
+
+} 
